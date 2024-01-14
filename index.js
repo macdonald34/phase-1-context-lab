7dbb2b6b058489df84f4d2eca44bd88bc6eb5ad3
macdonald34 <macdonald.muhavi@student.moringaschool.com>
2024-01-14 15:52:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -52,14 +52,7 @@ const wagesEarnedOnDate = function (date) {
   const numberOfHoursWorked = hoursWorkedOnDate.call(this, date);
   return numberOfHoursWorked * this.payPerHour;
 };
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
 
 const allWagesFor = function () {
   const eligibleDates = this.timeInEvents.map(function (e) {
@@ -71,7 +64,7 @@ const allWagesFor = function () {
       return memo + wagesEarnedOnDate.call(this, d);
     }.bind(this),
     0
-  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  ); 
 
   return payable;
 };
